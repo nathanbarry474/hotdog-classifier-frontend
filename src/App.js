b77f66c3bdcbde46bc8d94d6a44628f88870c5bd
Nathan Barry <Nathanbarry474@gmail.com>
2020-06-25 14:28:25 -0500

    Made it look better, added display photo function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState(null)
+  const [fileURL, setFileURL] = useState(null)
+  const [result, setResult] = useState(null)
+
+  const fileSelectedHandler = e => {
+    setSelectedFile(e.target.files[0])
+    setFileURL(URL.createObjectURL(e.target.files[0]))
+  }
+
+  const fileUploadHandler = e => {
+    let fd = new FormData();
+    fd.append('image', selectedFile)
+    axios.post('http://127.0.0.1:5000/predict', fd)
+    .then(res => setResult(res.data.class_name))
+    .catch(error => console.log(error))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2 className='title'><span role="img" aria-label="hotdog">🌭</span> Hotdog Or Not?</h2>
+      <input type='file' onChange={fileSelectedHandler} />
+      <button onClick={fileUploadHandler}>Upload</button>
+      <br/>
+      <img className='img' alt='Uploaded' src={fileURL} width='40%'/>
+      <h2>{result}</h2>
     </div>
   );
 }
